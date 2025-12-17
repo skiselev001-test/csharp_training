bf704779254a2da280c522e443a6370e58c9b534
Sergey Kiselev <skiselev001@gmail.com>
2025-12-17 17:26:04 +0300

    Rename to correct name

--- adressbook-web-tests/adressbook-web-tests/GroupCreationTests.cs
+++ adressbook-web-tests/adressbook-web-tests/GroupCreationTests.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace adressbook_web_tests
+namespace WebAddressbookTests
 {
     [TestFixture]
-    public class AdressbookTestCases
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace adressbook_web_tests
         }
 
         [Test]
-        public void CreateGroupTestCase()
+        public void GroupCreationTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Click();
