2bf4940bc7f4d9e65efc3e5ef98362bfbd19609d
Sergey Kiselev <skiselev001@gmail.com>
2025-12-30 16:59:42 +0300

    lesson 3.4 completed

--- adressbook-web-tests/adressbook-web-tests/appmanager/NavigationHelper.cs
+++ adressbook-web-tests/adressbook-web-tests/appmanager/NavigationHelper.cs
@@ -16,20 +16,39 @@ namespace WebAddressbookTests
         }
         public void OpenHomePage()
         {
+            if (driver.Url == baseURL)
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL);
         }
         public void GoToGroupsPage()
         {
+            if (driver.Url == baseURL + "/group.php"
+                && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
+
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
         public void GoToUserPage()
         {
+            if (driver.Url == baseURL + "/edit.php"
+                && IsElementPresent(By.Name("submit")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("add new")).Click();
         }
 
         public void GoToHomePage()
         {
+            if (driver.Url == baseURL)
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("home page")).Click();
         }
     }
