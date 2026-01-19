03f37491fb0ea46ad0465498f5166f1204265405
Sergey Kiselev <skiselev001@gmail.com>
2026-01-19 19:53:47 +0300

    task13 is completed

--- adressbook-web-tests/adressbook-web-tests/tests/UserCreationTests.cs
+++ adressbook-web-tests/adressbook-web-tests/tests/UserCreationTests.cs
@@ -13,10 +13,36 @@ namespace WebAddressbookTests
     [TestFixture]
     public class UserCreationTests : AuthTestBase
     {
-        [Test]
-        public void UserCreationTest()
+        public static IEnumerable<ContactData> RandomGroupDataProvider()
         {
-            ContactData userData = new ContactData("User1", "User1_lastname");
+            List<ContactData> users = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                users.Add(new ContactData(GenerateRandomString(20), GenerateRandomString(20))
+                {
+                    Middlename = GenerateRandomString(10),
+                    Address = GenerateRandomString(10),
+                    Email = GenerateRandomString(10),
+                    Email2 = GenerateRandomString(10),
+                    Email3 = GenerateRandomString(10),
+                    HomePhone = "(000)-1212-1212",
+                    MobilePhone = "(000)-1212-333",
+                    WorkPhone = "(000)-1212-44444",
+                    Bday = "17",
+                    Bmonth = "January",
+                    Byear = "2010",
+                    Aday = "27",
+                    Amonth = "January",
+                    Ayear = "2010"
+                });
+
+            }
+            return users;
+        }
+        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public void UserCreationTest(ContactData userData)
+        {
+            /*ContactData userData = new ContactData("User1", "User1_lastname");
             userData.Middlename = "User1_middlename";
             userData.Address = "TTbb";
             userData.Email = "test@test";
@@ -30,7 +56,7 @@ namespace WebAddressbookTests
             userData.Byear = "2010";
             userData.Aday = "27";
             userData.Amonth = "January";
-            userData.Ayear = "2010";
+            userData.Ayear = "2010";*/
 
             List<ContactData> oldUsers = app.Contacts.GetUserList();
             
