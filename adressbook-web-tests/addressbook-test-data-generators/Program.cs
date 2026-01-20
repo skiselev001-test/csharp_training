f9242ebd88fde24ff719a8ad675c99905b043c63
Sergey Kiselev <skiselev001@gmail.com>
2026-01-20 14:27:47 +0300

    lesson6.1 is completed

---
+++ adressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAddressbookTests;
+using System.IO;
+
+namespace addressbook_test_data_generators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter write = new StreamWriter(args[1]);
+
+            for (int i = 0; i < count; i++)
+            {
+                write.WriteLine(String.Format("${0},${1},${2}",
+                    TestBase.GenerateRandomString(10),
+                    TestBase.GenerateRandomString(10),
+                    TestBase.GenerateRandomString(10)));
+            }
+            write.Close();
+        }
+    }
+}
--- adressbook-web-tests/adressbook-web-tests/tests/GroupCreationTests.cs
+++ adressbook-web-tests/adressbook-web-tests/tests/GroupCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -26,9 +27,27 @@ namespace WebAddressbookTests
             return groups;
         }
 
+        public static  IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+
+            }
+
+            return groups;
+        }
         
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGropList();
